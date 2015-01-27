051076071f69ebb286f8544b2646146a23e74f08
Darren Shepherd <darren@rancher.com>
2015-01-26 23:47:42 -0700

    Run cadvisor with nsenter in logs dir as work around

--- apply.sh
+++ apply.sh
@@ -3,7 +3,7 @@
 source ${CATTLE_HOME:-/var/lib/cattle}/common/scripts.sh
 
 mkdir -p content-home/bin
-cp dist/cadvisor content-home/bin
+cp dist/{cadvisor,cadvisor.sh} content-home/bin
 chmod +x content-home/bin/cadvisor
 
 stage_files
